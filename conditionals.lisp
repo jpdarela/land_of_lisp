e17c6a17e97464a3ac2977bac120a743ed445e50
João Paulo Darela Filho <darelafilho@gmail.com>
2018-09-17 22:25:23 -0300

    scenery

--- conditionals.lisp
+++ conditionals.lisp
@@ -1,4 +1,4 @@
-;; this file contains the demonstrations based on the book 
+;; this file contains the demonstrations based on the book
 
 (defun conditionals ()
 
@@ -76,7 +76,64 @@
 	((< number 1) (setf *a* number) (princ "Smaller than One"))
 	(t (setf *a* number) (princ "Equal to One"))))
 
+(defun pudding-eater-2 (person)
+  (case person
+    ((henry) (setf *arch-enemy* 'lisp-alien)
+     '(Curse your Stupid Alien - you ate my pudding))
+    ((johnny) (setf *arch-enemy* 'old-johnny)
+     '(i hope you chocked on my pudding johnny))
+    (otherwise (setf *arch-enemy* person)
+	       '(why you eat my pudding stranger))))
 
 
+(defparameter *is-odd* '())
+(defparameter *is-even* '())
+
+(defun cond_tricks (number)
+  (or (evenp number) (setf *is-odd* t))
+  (or (oddp number) (setf *is-even* t)))
+
+(defun cond_tricks-2 (number)
+  (and (evenp number) (setf *is-even* t))
+  (and (oddp number) (setf *is-odd* t)))
+
+(defparameter *condition* nil)
+
+(defun test-and-trick (c)
+  "shortcut Boolean evaluation is the key to understand this idiom"
+  (setf *condition* c)
+  (and *condition* (princ "Saving something -") (princ " do something more")))
+
+(defun test-or-trick (c)
+  "shortcut Boolean evaluation is the key to understand this idiom"
+  (setf *condition* c)
+  (or *condition* (princ "do something")))
+
+(find-if #'null '(1 2 3 4 nil))
+(find-if #'oddp '(6 2 4 5))
+
+#|| 
+
+Comparing stuff
+
+1 - Use eq to compare symbols
+2 - Use equal for everthing else
+3 - eql  = eq + handles comparisons between numbers ans characters
+4 - equalp = equal improved. Can handle comparisons between integers
+    and floats and upper and lower case strings 
+
+||#
+
+(eq 'apple 'apple)
+(eq (cons 1 (cons 2 (cons 3 '()))) '(1 2 3))
+(eql 1 1)
+(equal (cons 1 (cons 2 (cons 3 '()))) '(1 2 3))
+(equalp "Bob" "bob")
+
+
+(defparameter *apple* 'apple)
+(cond ((eq *apple* 'apple) 'apple)
+      ((eq *apple* 'orange) 'orange)
+      (t 'yupi))
 
 
---
+++ scenery.lisp
@@ -0,0 +1,51 @@
+;; code for the wizard's adventures - Chapter 5
+
+;; Nodes of our game graph - alist
+(defparameter *nodes* '((living-room (you are in the living-room.
+				      a wizard is snoring loudly on the couch.))
+			(garden (you are in the beaultiful garden.
+				 there is a well in front of you.))
+			(attic (you are in the attic.
+				there is a giant welding torch in the corner.))))
+
+(defparameter *edges* '((living-room (garden west door)
+			 (attic upstairs ladder))
+			(garden (living-room east door))
+			(attic (living-room downstairs ladder))))
+
+
+;; function that returns the scenery graph nodes
+(defun describe-location (location nodes)
+  (cadr (assoc location nodes)))
+
+(defun describe-path (edge)
+  `(there is a ,(caddr edge) going ,(cadr edge) from here.))
+
+(defun describe-paths (location edges)
+  (apply #'append (mapcar #'describe-path (rest (assoc location edges)))))
+
+
+;; objects
+(defparameter *objects* '(whiskey bucket frog chain))
+
+(defparameter *object-locations* '((whiskey living-room)
+				  (bucket living-room)
+				  (chain garden)
+				  (frog garden)))
+
+(defun objects-at (loc objs obj-locs)
+  (labels ((at-loc-p (obj)
+	     (eq (cadr (assoc obj obj-locs)) loc)))
+    (remove-if-not #'at-loc-p objs)))
+
+(defun describe-objects (loc objs obj-loc)
+  (labels ((describe-obj (obj)
+	     `(you see a ,obj on the floor.)))
+    (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))
+
+;; game init
+
+(defparameter *location* 'living-room)
+
+(defun look ()
+  (append ()))
