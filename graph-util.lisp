46317f1d7dfab1d07c1c902c22b03223328feb6f
João Paulo Darela Filho <darelafilho@gmail.com>
2019-10-03 16:01:34 +0200

    changes to test git kraken
    erase scenery.lisp and

--- graph-util.lisp
+++ graph-util.lisp
@@ -1,15 +1,4 @@
-(defparameter *wizard-nodes* '((living-room (you are in the living room.
-					     a wizard is snoring loud on the couch.))
-
-			       (garden (you are in the beaultful garden.
-					there is a well in front of you.))
-			       (attic (you are in tthe attic. there is
-				       a giant welding torch in the corner.))))
-
-(defparameter *wizard-edges* '((living-room (garden west door)
-				(attic upstairs ladder))
-			       (garden (living-room east door))
-			       (attic (living-room downstairs ladder))))
+;; plot graph utilities
 
 
 (defun dot-name (exp)
